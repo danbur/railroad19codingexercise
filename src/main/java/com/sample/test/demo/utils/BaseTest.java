e7767e72800a4004f865637f688ca96741545573
Daniel Burton <danielburton5@gmail.com>
2020-05-10 12:57:56 -0600

    Add Lombok and Spring dependency injection

--- src/main/java/com/sample/test/demo/utils/BaseTest.java
+++ src/main/java/com/sample/test/demo/utils/BaseTest.java
@@ -1,6 +1,10 @@
 package com.sample.test.demo.utils;
 
 
-public class BaseTest {
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+
+@ContextConfiguration(locations = { "classpath:spring-test-config.xml" })
+public class BaseTest extends AbstractTestNGSpringContextTests {
 
 }
